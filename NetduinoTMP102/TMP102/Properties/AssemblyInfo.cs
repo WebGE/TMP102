91d883cf0555b5ef396de93a8f5397cc682f1404
WebGE <philippemariano@hotmail.fr>
2015-03-21 15:35:41 +0100

    Révision mineure

--- NetduinoTMP102/TMP102/Properties/AssemblyInfo.cs
+++ NetduinoTMP102/TMP102/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MFClassLibrary1")]
+[assembly: AssemblyTitle("TMP102")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MFClassLibrary1")]
+[assembly: AssemblyProduct("TMP102")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
