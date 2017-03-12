c21b533895b52f7b0f4708292aaa06a3a49653a4
Mariano Philippe <philippemariano@hotmail.fr>
2017-03-12 09:30:34 +0100

    updating

---
+++ NetduinoTMP102/FezPanda/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using GHI.Pins;
+
+
+namespace FezPanda
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.Print(Resources.GetString(Resources.StringResources.String1));
+        }
+    }
+}
